df903f2ee1a3c072e37e598a1073cd4206e4fc54
jay <pediredlajayaprakash@gmail.com>
2026-07-15 23:04:09 +0530

    Initial commit

---
+++ src/test/java/com/AI/Intelligent_Deposit_Recommendation_Engine/IntelligentDepositRecommendationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI.Intelligent_Deposit_Recommendation_Engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentDepositRecommendationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
