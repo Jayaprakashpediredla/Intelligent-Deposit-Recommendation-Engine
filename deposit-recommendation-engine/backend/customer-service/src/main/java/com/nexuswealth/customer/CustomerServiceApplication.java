6c3e90283aa1b20bf570b9160dbe8fd6a2c75ad4
jay <pediredlajayaprakash@gmail.com>
2026-07-30 01:14:57 +0530

    bootstrap customer service module

--- deposit-recommendation-engine/backend/customer-service/src/main/java/com/nexuswealth/customer/CustomerServiceApplication.java
+++ deposit-recommendation-engine/backend/customer-service/src/main/java/com/nexuswealth/customer/CustomerServiceApplication.java
@@ -2,20 +2,11 @@ package com.nexuswealth.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
-
-    @GetMapping({"/", "/health"})
-    public String health() {
-        return "Customer service is running";
-    }
 }
-
