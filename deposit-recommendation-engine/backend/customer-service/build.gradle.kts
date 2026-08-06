6989491f231e87c693f1a1f96888ef2885271654
jay <pediredlajayaprakash@gmail.com>
2026-08-06 23:58:21 +0530

    verify customer API integration

--- deposit-recommendation-engine/backend/customer-service/build.gradle.kts
+++ deposit-recommendation-engine/backend/customer-service/build.gradle.kts
@@ -15,16 +15,22 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation("org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
+    //Flyway
     implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
 
+    //PostgreSQL
     runtimeOnly("org.postgresql:postgresql")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
+    //Lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
